10ccbc36782b894cb1c0e3a2e9291b1843a5abc5
Jhovanny <jhovanny.fragoso@codiceglobal.com>
2024-04-05 09:22:11 -0600

    Feat: slice

--- src/redux/userSlice.ts
+++ src/redux/userSlice.ts
@@ -2,7 +2,7 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
     name: "",
-    userName: "",
+    username: "",
     reducers: {}
 }
 
@@ -10,11 +10,23 @@ export const userSlice = createSlice({
     name: "user",
     initialState,
     reducers: {
-        addUser: (state, action) = {
-            const {name, username, email } = action.payload;
-            state.name = name;
-            state.username = username;
-            state.email = email;
+        // addUser: (state, action) = {
+        //     const {name, username, email } = action.payload;
+        //     state.name = name;
+        //     state.username = username;
+        //     state.email = email;
+        // },
+
+
+        addUser: (state, action) => {
+            const { name, username, email } = action.payload;
+            return {
+                ...state,
+                name,
+                username,
+                email,
+            };
         },
+        
     }
 })
